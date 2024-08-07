6c55aa106a64678fbcb55f894dc768f6b0d13e1c
Gleriston <xpanium@gmail.com>
2024-08-06 22:35:34 -0300

    add first app spec/test

---
+++ src/App.spec.tsx
@@ -0,0 +1,10 @@
+describe("Testa o componente App", () => {
+  
+  test("1 + 1 deve ser 2", () => {
+    const sum = 1 + 1;
+
+    expect(sum).toBe(2);
+  });
+
+  test("Deve haver um título na página", () => {});
+});
