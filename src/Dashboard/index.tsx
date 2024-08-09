8fc25f636b9a59828d4f435f5ee3ece5ca8a49ce
Gleriston <xpanium@gmail.com>
2024-08-08 23:04:38 -0300

    add new file main  dashboard

---
+++ src/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./styles.module.scss"
+
+export default function Dashboard() {
+  return (
+    <div className={styles.container}>
+      <h2>Dashboard</h2>
+      
+    </div>
+  );
+}
